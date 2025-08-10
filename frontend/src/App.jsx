02ceda5f078afbfb932270f0c0370e2301c46ffb
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-08-10 14:34:25 -0700

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div className='card'>
+            <h1>Welcome to Techyjaunt Alumni</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
