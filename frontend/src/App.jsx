72e1d27e0178a1cfb9dd309d189f2e9d4b3b0f22
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2025-09-03 22:00:24 +0100

    chore:Added color theme and  istok-web font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline text-error">
         certificate-verification-system
       </h1>
     </div>
