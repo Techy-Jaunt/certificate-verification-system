19a34a58f39299ee822998be4219c6b27d9d2823
Onu Wisdom <onuwisdom2000@gmail.com>
2025-09-08 17:38:27 +0100

    worked on the certificate preview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,17 @@ import Home from './pages/Home';
 import RecruiterSearch from './pages/RecruiterSearch';
 import CertificateRequest from './pages/CertificateRequest';
 import CertificatePreview from './pages/CertificatePreview';
-import CertificateVerificationPreview from './components/CertificateVerificationPreview';
-import RecruiterCertificatePreview from './components/RecruitercertificatePreview';
 
 function AppRoutes() {
 	return (
-		// <Router>
-		//   <Routes>
-		//     <Route path="/" element={<Home />} />
-		//     <Route path="/recruiter-search" element={<RecruiterSearch />} />
-		//     <Route path="/certificate-request" element={<CertificateRequest />} />
-		//     <Route path="/certificate-verified" element={<CertificatePreview />} />
-		//   </Routes>
-		// </Router>
-		<div>
-			{/* <CertificateVerificationPreview /> */}
-			<RecruiterCertificatePreview />
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/recruiter-search" element={<RecruiterSearch />} />
+				<Route path="/certificate-request" element={<CertificateRequest />} />
+				<Route path="/certificate-verified" element={<CertificatePreview />} />
+			</Routes>
+		</Router>
 	);
 }
 
