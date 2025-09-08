b92c41de07e1a0be7631e288670d6b871b013181
Onu Wisdom <onuwisdom2000@gmail.com>
2025-09-08 17:37:21 +0100

    worked on the certificate preview page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router";
-import Home from "./pages/Home";
-import RecruiterSearch from "./pages/RecruiterSearch";
-import CertificateRequest from "./pages/CertificateRequest";
-import CertificatePreview from "./pages/CertificatePreview";
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import RecruiterSearch from './pages/RecruiterSearch';
+import CertificateRequest from './pages/CertificateRequest';
+import CertificatePreview from './pages/CertificatePreview';
+import CertificateVerificationPreview from './components/CertificateVerificationPreview';
+import RecruiterCertificatePreview from './components/RecruitercertificatePreview';
 
 function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recruiter-search" element={<RecruiterSearch />} />
-        <Route path="/certificate-request" element={<CertificateRequest />} />
-        <Route path="/certificate-verified" element={<CertificatePreview />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		// <Router>
+		//   <Routes>
+		//     <Route path="/" element={<Home />} />
+		//     <Route path="/recruiter-search" element={<RecruiterSearch />} />
+		//     <Route path="/certificate-request" element={<CertificateRequest />} />
+		//     <Route path="/certificate-verified" element={<CertificatePreview />} />
+		//   </Routes>
+		// </Router>
+		<div>
+			{/* <CertificateVerificationPreview /> */}
+			<RecruiterCertificatePreview />
+		</div>
+	);
 }
 
 export default AppRoutes;
