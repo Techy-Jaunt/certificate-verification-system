e5d1b6457a1910c89a26dd6dfde48ef20d24ed4a
tommysin6 <amosokusanya11@gmail.com>
2025-09-09 00:14:43 +0100

    Added a responsive Certificate Request Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import RecruiterSearch from "./pages/RecruiterSearch";
 import CertificateRequest from "./pages/CertificateRequest";
