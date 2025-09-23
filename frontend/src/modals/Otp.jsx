a8f03863a15e2fdf480c9f30eabc1b18b00db40a
suruaino <studymail4suru@gmail.com>
2025-09-23 18:41:48 +0100

    Fixed minor testing logics

--- frontend/src/modals/Otp.jsx
+++ frontend/src/modals/Otp.jsx
@@ -7,8 +7,8 @@ const Otp = () => {
   const [verifyOtp, setVerifyOtp] = useState("Verify Otp");
 
   // Just Testing the different states with the two expressions immediately below
-  const [ otpOk, setOtpOk ] = useState(true);
-  const [ isSuccess, setIsSuccess ] = useState(true);
+  const [otpOk, setOtpOk] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(true);
 
   const [otp, setOtp] = useState("");
   const isValidOtp = /^\d{6}$/.test(otp);
@@ -22,18 +22,17 @@ const Otp = () => {
   };
 
   const handleVerifyBtnClick = () => {
-   // Just Testing the different states with the two expressions immediately below
-    setOtpOk(prev => !prev);
-    setIsSuccess(prev => !prev);
-
     setVerifyOtp("Verifying....");
 
     if (timerRef.current) clearTimeout(timerRef.current);
 
     timerRef.current = setTimeout(() => {
+      // Just Testing the different states with the two expressions immediately below
+      setOtpOk(true);
+      setIsSuccess((prev) => !prev);
       setVerifyOtp("Verify Otp");
       timerRef.current = null;
-    }, 3000);
+    }, 1000);
   };
 
   useEffect(() => {
@@ -56,7 +55,9 @@ const Otp = () => {
             </button>
 
             <h3 className="font-semibold">OTP Verification</h3>
-            <p className="text-[var(--color-darker)]">Enter the 6-digit pin sent to your email</p>
+            <p className="text-[var(--color-darker)]">
+              Enter the 6-digit pin sent to your email
+            </p>
             <div className="input-box w-full  flex flex-col gap-2">
               <label htmlFor="" className="font-semibold">
                 OTP Code
@@ -68,13 +69,23 @@ const Otp = () => {
                 placeholder="000000"
                 className="h-8 text-center border rounded-md"
               />
-              <div className={`${otpOk? "bloxk" : "hidden"}`}>
-              {isSuccess? (<span className="w-max bg-[#E1F7E2] text-[var(--color-success)] px-2 rounded-lg">Verification complete</span>) : (<span className="w-max bg-[#FCE1E1] text-[var(--color-error)] px-2 rounded-lg">Incorrect Otp</span>)}
+              <div className={`${otpOk ? "block" : "hidden"}`}>
+                {isSuccess ? (
+                  <span className="w-max bg-[#E1F7E2] text-[var(--color-success)] px-2 rounded-lg">
+                    Verification complete
+                  </span>
+                ) : (
+                  <span className="w-max bg-[#FCE1E1] text-[var(--color-error)] px-2 rounded-lg">
+                    Incorrect Otp
+                  </span>
+                )}
               </div>
               <button
                 disabled={!isValidOtp}
                 className={`${
-                  isValidOtp ? "bg-[var(--color-primary-400)]" : "bg-[var(--color-dark-active)]"
+                  isValidOtp
+                    ? "bg-[var(--color-primary-400)]"
+                    : "bg-[var(--color-dark-active)]"
                 } text-white h-8 rounded-md ${
                   isValidOtp ? "cursor-pointer" : "cursor-auto"
                 }`}
