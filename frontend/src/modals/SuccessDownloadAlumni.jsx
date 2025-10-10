302199ddccb482133326776cd8c2d1ce7ac75d13
Frontend-lover <116767544+suruaino@users.noreply.github.com>
2025-10-10 07:54:36 +0100

    Fix/verify graduate (#46)
    * Started api integration
    
    * fixed graduate verification
    
    * Fixed the verify graduate and all it linkages to the end from navbar to cert-preview
    
    * Did code clean up
    
    * Added Certificate image to the previews
    
    * Changed api base url and fixed some typos
    
    * fixed search form width and paddings

--- frontend/src/modals/SuccessDownloadAlumni.jsx
+++ frontend/src/modals/SuccessDownloadAlumni.jsx
@@ -1,9 +1,8 @@
 import successIcon from "/icons/tick.png";
 import { useEffect } from "react";
 
-const SuccessDownloadAlumni = ({ closeSuccessModal}) => {
-
-    useEffect(() => {
+const SuccessDownloadAlumni = ({ closeSuccessModal }) => {
+  useEffect(() => {
     document.body.style.overflow = "hidden"; // lock scroll
     return () => {
       document.body.style.overflow = "auto"; // restore when modal closes
@@ -11,18 +10,16 @@ const SuccessDownloadAlumni = ({ closeSuccessModal}) => {
   }, []);
 
   const handleClose = () => {
-    closeSuccessModal()
+    closeSuccessModal();
   };
   return (
-    <div
-      className='fixed z-40 inset-0 flex items-center justify-center bg-black/50'
-    >
+    <div className="fixed z-40 inset-0 flex items-center justify-center bg-black/50">
       <div className="rounded-xl flex flex-col  justify-center items-center bg-white w-[20rem] md:max-w-[28rem] lg:max-w-[35rem] md:w-full p-6 h-fit md:h-[20rem] lg:h-[25rem] px-16 scale-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:-translate-y-27 lg:-translate-y-43">
         <div className="flex items-center justify-center w-20 h-20 mx-auto  mb-4 border-5 border-(--color-success) rounded-full">
           <img src={successIcon} alt="" className="w-10" />
         </div>
 
-        <h2 className="text-xl font-semibold text-gray-800 text-gray-100 text-center">
+        <h2 className="text-xl font-semibold text-gray-800 text-center">
           Download Successful
         </h2>
 
@@ -33,13 +30,12 @@ const SuccessDownloadAlumni = ({ closeSuccessModal}) => {
 
         <div className="mt-6 flex flex-col items-center gap-3">
           <button
-                        onClick={handleClose}
-                        className="absolute top-6 md:top-8 right-5 md:right-8 cursor-pointer text-gray-500 hover:text-gray-700 text-large ml-4"
-                        type="button"
-                    >
-                        ✕
-                    </button>
-
+            onClick={handleClose}
+            className="absolute top-6 md:top-8 right-5 md:right-8 cursor-pointer text-gray-500 hover:text-gray-700 text-large ml-4"
+            type="button"
+          >
+            ✕
+          </button>
         </div>
       </div>
     </div>
